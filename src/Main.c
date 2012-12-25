dc80069dbade131b8d647312b1db01791e049c46
Nehmulos <nehmu@lavabit.com>
2012-12-25 19:35:17 +0100

    put env variable for sdl to center the window;

--- src/Main.c
+++ src/Main.c
@@ -7,8 +7,10 @@
 
 #include "Constants.h"
 #include "GameEngine.h"
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
+	putenv("SDL_VIDEO_CENTERED=1");
 	GameEngine* engine = GameEngine_create();
 	return GameEngine_run(engine);;
 }
