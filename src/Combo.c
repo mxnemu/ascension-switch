9b7aa2350922cb71c5ab63b356e2d037959ac0d9
Nehmulos <nehmu@lavabit.com>
2013-01-12 00:10:09 +0100

    added Combo data-modell stuff;

---
+++ src/Combo.c
@@ -0,0 +1,17 @@
+#include "Combo.h"
+#include "Utils.h"
+
+Combo* Combo_create() {
+	Combo* this = malloc(sizeof(Combo));
+	this->name = "";
+	this->action = none;
+	this->followups = Vector_Create();
+	this->timeUntilReady = 0;
+	this->timeUntilCancel = 2000;
+	return this;
+}
+
+void Combo_destroy(Combo* this) {
+	FREE_VECTOR_WITH_ELMENTS(this->followups, Combo_destroy);
+	free(this);
+}
---
+++ src/Combo.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Constants.h"
+#include "Input.h"
+#include "Vector.h"
+
+typedef struct Combo {
+	const char* name;
+	ActionId action;
+	Vector* followups; // next combos
+	RawTime timeUntilReady; // action will not be accepted before this time passed
+	RawTime timeUntilCancel; // reset the combo tree when this time passed
+} Combo;
+
+Combo* Combo_create();
+void Combo_destroy(Combo* this);
