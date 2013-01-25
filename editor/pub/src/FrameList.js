55d10f854e706240da7ed5495e99d9bf5c0cb428
Nehmulos <nehmu@lavabit.com>
2013-01-26 00:36:25 +0100

    list frames in html;

--- editor/pub/src/FrameList.js
+++ editor/pub/src/FrameList.js
@@ -4,4 +4,14 @@ function FrameList(entity) {
 
 FrameList.prototype.addFrame = function(frame) {
     this.entity.frames.push(frame);
+    this.addHtmlNode(frame);
+}
+
+FrameList.prototype.addHtmlNode = function(frame) {
+    frame.node = $("<li class='frame'></li>");
+    frame.nameNode = $("<span class='name'></span>");
+    frame.nameNode.text("frame " + $.inArray(frame, this.entity.frames));
+    frame.node.append(frame.nameNode);
+
+    $(".frameList").append(frame.node);
 }
