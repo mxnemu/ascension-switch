30e0ae7d46238836806c38b7f76f53c939822b32
Nehmulos <nehmu@lavabit.com>
2013-01-26 15:34:09 +0100

    call frame select after a new frame was created;

--- editor/pub/src/FrameSelector.js
+++ editor/pub/src/FrameSelector.js
@@ -21,6 +21,7 @@ function FrameSelector(sidebar) {
             if (_this.currentSelection.validate()) {        
                 _this.entity.frames.push(_this.currentSelection);
                 _this.sidebar.addFrame(_this.currentSelection);
+                _this.sidebar.selectFrame(_this.currentSelection);
             }
             _this.currentSelection = null; 
         } else {
