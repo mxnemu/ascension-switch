71b788576b93478ce2e1794ff8417d1ff64a3114
Nehmulos <nehmu@lavabit.com>
2013-01-27 21:17:04 +0100

    mark the selected frame gray;

--- editor/pub/src/FrameList.js
+++ editor/pub/src/FrameList.js
@@ -1,10 +1,21 @@
 function FrameList(entity) {
     this.entity = entity;
+    this.selectedFrame = null;
 }
 
 FrameList.prototype.addFrame = function(frame) {
     this.addHtmlNode(frame);
 }
+
+
+FrameList.prototype.rebuildHtmlTree = function() {
+    $(".frameList").empty();
+    for (var i=0; i < this.enttiy.frames.length; ++i) {
+        this.addHtmlNode(this.enttiy.frames[i]);
+    }
+    this.setHtmlSelection();
+}
+
 
 FrameList.prototype.addHtmlNode = function(frame) {
     frame.node = $("<li class='frame'></li>");
@@ -13,4 +24,13 @@ FrameList.prototype.addHtmlNode = function(frame) {
     frame.node.append(frame.nameNode);
 
     $(".frameList").append(frame.node);
+    this.selectedFrame = frame;
+    this.setHtmlSelection()
+}
+
+FrameList.prototype.setHtmlSelection = function() {
+    $(".frameList .frame .name").removeClass("selected");
+    if (this.selectedFrame) {
+        this.selectedFrame.nameNode.addClass("selected");
+    }
 }
